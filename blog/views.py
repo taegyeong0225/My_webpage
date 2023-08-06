9e3e1aeeea98bbeb228c461e09216e5ddce44161
taegyeong0225 <202244042@itc.ac.kr>
2023-08-06 17:04:43 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+
+    # QuerySet 출력
+    # print(posts)
+
+    return render(
+        request,
+        'blog/index.html', {
+            'post': posts
+        }
+    )
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+
+    return render(
+        request,
+        'single_pages/single_post_page.html',
+    )
