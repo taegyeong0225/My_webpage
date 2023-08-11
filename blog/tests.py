e77a2ce4183de107175ad80a45a589f8c4dc4f75
taegyeong0225 <202244042@itc.ac.kr>
2023-08-11 15:41:35 +0900

    내비게이션 바와 ㅜ=풭 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -8,6 +8,22 @@ class TestView(TestCase):
         # 기본적으로 설정되어야 하는 내용, Client()를 사용하겠다.
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navBar = soup.nav
+        self.assertIn('Blog', navBar.text)
+        self.assertIn('About me', navBar.text)
+
+        logo_btn = navBar.find('a', text='태경의 웹 패이지')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navBar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navBar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navBar.find('a', text='About me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
 
     def test_post_list(self):
         # 1.1 포스트 목록 페이지를 가져온다.
@@ -18,10 +34,9 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
         # 1.4 네비게이션 바가 있다.
-        navBar = soup.nav
+        self.navbar_test(soup)
         # 1.5 Blog, About Me라는 문구가 내비게이션 바에 있다.
-        self.assertIn('Blog',navBar.text)
-        self.assertIn('About me', navBar.text)
+            # navbar_test로 이동
 
         # 2.1 메인 영역에 게시글이 하나도 없다면
         self.assertEqual(Post.objects.count(), 0)
@@ -65,9 +80,7 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다
-        navBar = soup.nav
-        self.assertIn('Blog', navBar.text)
-        self.assertIn('About me', navBar.text)
+        self.navbar_test(soup)
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다
         self.assertIn(post_001.title, soup.title.text)
         # 2.4 첫 번째 포스트의 제목이 포스트 영역에 있다
@@ -78,3 +91,4 @@ class TestView(TestCase):
 
         # 2.6 첫 번째 포스트의 내용(content)이 포스트 영역에 있다
         self.assertIn(post_001.content, post_area.text)
+
